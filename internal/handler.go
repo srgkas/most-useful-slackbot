b601a200fab7fa50ca65c79d074450dba748ddf1
m1x0n <mmorozovm@gmail.com>
2021-05-20 22:18:57 +0300

    Added message example.

--- internal/handler.go
+++ internal/handler.go
@@ -20,6 +20,7 @@ func ReplyInHotfixThread(event slack.Event) error {
 }
 
 func ReleaseTag(event slack.Event) error {
-	//
+	// String example to parse
+	// Succeeded - AirSlate / PROD Env / PROD: Builds / Backend: API / api-addons / Deploy #173 | - v8.11.1 [v8.11.1]>
 	return nil
 }
