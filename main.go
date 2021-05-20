bde293642f003a5001404565ab2a6eaf0b74b8a2
Kasianchuk Serhii <kasianchuk.serhii@pdffiller.team>
2021-05-20 19:18:09 +0300

    Added challenge handling

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -9,7 +12,26 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/events/handle", func (w http.ResponseWriter, r *http.Request) {
+		var err error
+		all, _ := ioutil.ReadAll(r.Body)
 
+		// handle challenge request
+		var data struct {
+			Type string
+			Token string
+			Challenge string `json:"challenge"`
+		}
+
+		fmt.Println(string(all))
+		err = json.Unmarshal(all, &data)
+
+		if err == nil && data.Challenge != "" {
+			fmt.Println("Challenge accepted")
+			w.Write([]byte(data.Challenge))
+			return
+		}
+
+		// event parsing goes here
 	})
 
 	http.ListenAndServe(":8000", r)
