508026f36af83da2b72452f58a919a5609c3900f
m1x0n <mmorozovm@gmail.com>
2021-05-20 19:29:57 +0300

    Added dummy implementation.

--- internal/gh/releaser.go
+++ internal/gh/releaser.go
@@ -1,35 +1,51 @@
 package gh
 
 import (
-	"context"
 	"github.com/google/go-github/v35/github"
-	"golang.org/x/oauth2"
 	"log"
 )
 
+// Release DTO
+
+type Release struct {
+	Repo string
+	Tag string
+}
+
+func NewRelease(repo string, tag string) *Release {
+	return &Release{Repo: repo, Tag: tag}
+}
+
+// Releaser code
+
 type Releaser interface {
-	Release(repo string, tag string) error
+	Release(r *Release) error
 }
 
 type GithubReleaser struct {
 	client *github.Client
 }
 
-func (r GithubReleaser) Release(repo string, tag string) error {
-	log.Printf("Released version: %s:%s\n", repo, tag)
+func (releaser GithubReleaser) Release(r *Release) error {
+	log.Printf("Released version: %s:%s\n", r.Repo, r.Tag)
 
 	return nil
 }
 
 func CreateReleaser(token string) Releaser {
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token},
-	)
+	// Testing
+	return &GithubReleaser{client: nil}
 
-	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
-
-	return &GithubReleaser{client: client}
+	//ctx := context.Background()
+	//ts := oauth2.StaticTokenSource(
+	//	&oauth2.Token{AccessToken: token},
+	//)
+	//
+	//tc := oauth2.NewClient(ctx, ts)
+	//
+	//client := github.NewClient(tc)
+	//
+	//return &GithubReleaser{client: client}
 }
+
+// Release should be done in go routines
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/srgkas/most-useful-slackbot/internal/gh"
+)
 
+func main() {
+	fmt.Println("Hello, bitches!")
+
+	release := gh.NewRelease("https://github.com/my-repo", "v1.0.0")
+	releaser := gh.CreateReleaser("token")
+
+	err := releaser.Release(release)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
